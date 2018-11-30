e9551c2e36940c79a98174dc9aacc74485c28aba
Federico Bruni <fede@inventati.org>
2018-11-30 17:17:27 +0100

    move .otf URW font files download to setup.sh
    This download will be necessary until fonts-urw-base35 package
    in Debian will include also .otf files.
    
    Restructure the setup.sh file to work better if run more
    than once.

--- mkosi/debian/mkosi.extra/etc/skel/setup.sh
+++ mkosi/debian/mkosi.extra/etc/skel/setup.sh
@@ -1,39 +1,77 @@
 #!/bin/bash
-printf "This wizard will help you to setup your Git configuration.\n\n"
 
-if [ -f ~/.gitconfig ]; then
-  printf "A file configuration already exists. If you proceed, it will be overwritten.\nPress Ctrl+C to cancel/Press enter to proceed: "
-  read _
+###  GIT CONFIGURATION  ###
+if [ ! -f ~/.gitconfig ];
+then
+  echo "This wizard will help you to setup your Git configuration."
+  echo -n "Please enter your name and surname: "
+  read NAME
+  git config --global user.name "$NAME"
+  echo -n "Please enter your email address: "
+  read EMAIL
+  git config --global user.email "$EMAIL"
+  echo "Your commit messages will be signed as '$NAME <$EMAIL>'."
+else
+  echo "Git is already configured. Skipping..."
+  echo "Remove ~/.gitconfig to force a new configuration."
 fi
 
-echo -n "Please enter your name and surname: "
-read NAME
-git config --global user.name "$NAME"
-echo -n "Please enter your email address: "
-read EMAIL
-git config --global user.email "$EMAIL"
-echo "Your commit messages will be signed as '$NAME <$EMAIL>'."
 
-git config --global color.ui auto
-echo
-
-# If this script is run again after first configuration,
-# print a warning and offer the option to exit.
-if [ -d ~/git-cl -a ~/lilypond-git -a ~/lilypond-extra ]; then
-  printf "You've already downloaded the repositories. Press Ctrl+C close the wizard: "
-read _
+###  DOWNLOAD REPOSITORIES  ###
+if [ ! -d ~/git-cl -a ~/lilypond-git -a ~/lilypond-extra ];
+then
+  echo "Now we'll download the repositories needed to contribute to LilyPond development. Proceed only if you have a working Internet connection."
+  read -p "Press Enter to continue. "
+  cd $HOME
+  echo "Cloning in your home directory: `pwd`. It will take a few minutes."
+  echo "Downloading git-cl repository..."
+  git clone git://github.com/gperciva/git-cl.git
+  echo "Downloading lilypond-extra repository..."
+  git clone git://github.com/gperciva/lilypond-extra/
+  echo "Downloading lilypond-git repository..."
+  git clone git://git.sv.gnu.org/lilypond.git lilypond-git
+else
+  echo "Repositories already downloaded. Skipping..."
+  echo
 fi
 
-echo "Now we'll download the repositories needed to contribute to LilyPond development. Proceed only if you have a working Internet connection."
-read -p "Press Enter to continue. "
-cd $HOME
-echo "Cloning in your home directory: `pwd`. It will take a few minutes."
-echo "Downloading git-cl repository..."
-git clone git://github.com/gperciva/git-cl.git
-echo "Downloading lilypond-extra repository..."
-git clone git://github.com/gperciva/lilypond-extra/
-echo "Downloading lilypond-git repository..."
-git clone git://git.sv.gnu.org/lilypond.git lilypond-git
 
+###  DOWNLOAD OTF URW FONTS  ###
+# LilyPond docs need Greek and Cyrillic fonts from Ghostscript URW35.
+# However the Debian package fonts-urw-base35 (currently in testing/buster)
+# does not include OpenType font files. So we have to download these files
+# from ghostscript repository.
+URW35FONTS=\
+"C059-BdIta.otf
+C059-Bold.otf
+C059-Italic.otf
+C059-Roman.otf
+NimbusMonoPS-Bold.otf
+NimbusMonoPS-BoldItalic.otf
+NimbusMonoPS-Italic.otf
+NimbusMonoPS-Regular.otf
+NimbusSans-Bold.otf
+NimbusSans-BoldOblique.otf
+NimbusSans-Oblique.otf
+NimbusSans-Regular.otf"
+
+# Download each font file
+if [ ! -d ~/.local/share/fonts ];
+then
+  for font in $URW35FONTS
+  do
+    # The URL GET parameter f=$font will be replaced with the font file name
+    WGETURL="http://git.ghostscript.com/?p=urw-core35-fonts.git;a=blob;hb=79bcdfb34fbce12b592cce389fa7a19da6b5b018;f=$font;"
+    # wget options: less verbose, create directory, don't make host directory,
+    # use file name from HTTP header, set download location
+    wget -nv -x -nH --content-disposition -P ~/.local/share/fonts "$WGETURL"
+  done
+else
+  echo "Fonts already downloaded. Skipping..."
+  echo
+fi
+
+
+###  EXIT  ###
 echo "Configuration completed successfully!"
-read -p "Press Enter to close the wizard."
+exit 0
