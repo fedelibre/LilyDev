c513140967c81895c3448946dff7658b87729a19
Federico Bruni <fede@inventati.org>
2020-05-11 15:10:03 +0200

    Use fonts-urw-base35 package in Debian

--- mkosi/debian/mkosi.extra/etc/skel/setup.sh
+++ mkosi/debian/mkosi.extra/etc/skel/setup.sh
@@ -38,43 +38,6 @@ else
 fi
 
 
-###  DOWNLOAD OTF URW FONTS  ###
-# LilyPond docs need Greek and Cyrillic fonts from Ghostscript URW35.
-# However the Debian package fonts-urw-base35 (currently in testing/buster)
-# does not include OpenType font files. So we have to download these files
-# from ghostscript repository.
-URW35FONTS=\
-"C059-BdIta.otf
-C059-Bold.otf
-C059-Italic.otf
-C059-Roman.otf
-NimbusMonoPS-Bold.otf
-NimbusMonoPS-BoldItalic.otf
-NimbusMonoPS-Italic.otf
-NimbusMonoPS-Regular.otf
-NimbusSans-Bold.otf
-NimbusSans-BoldOblique.otf
-NimbusSans-Oblique.otf
-NimbusSans-Regular.otf"
-
-# Download each font file
-if [ ! -d ~/.local/share/fonts ];
-then
-  echo "Download Greek and Cyrillic fonts from Ghostscript URW35..."
-  for font in $URW35FONTS
-  do
-    # The URL GET parameter f=$font will be replaced with the font file name
-    WGETURL="http://git.ghostscript.com/?p=urw-core35-fonts.git;a=blob;hb=79bcdfb34fbce12b592cce389fa7a19da6b5b018;f=$font;"
-    # wget options: less verbose, create directory, don't make host directory,
-    # use file name from HTTP header, set download location
-    wget -nv -x -nH --content-disposition -P ~/.local/share/fonts "$WGETURL"
-  done
-else
-  echo "Fonts already downloaded. Skipping..."
-  echo
-fi
-
-
 ###  EXIT  ###
 echo "Configuration completed successfully!"
 exit 0
